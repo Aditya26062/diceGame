0a90444042beef7cdcd4d9a783bab3e574c6c72b
Aditya Shukla <shukla.aditya.26062000@gmail.com>
2022-05-11 17:09:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+random1 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src",("images/dice"+random1+".png"));
+
+random2 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img2").setAttribute("src",("images/dice"+random2+".png"));
+
+if(random1>random2){
+    document.querySelector("h1").textContent = "🚩 Player 1 wins";
+}
+else if(random1<random2){
+    document.querySelector("h1").textContent = " Player 2 wins 🚩";
+}
+else{
+    document.querySelector("h1").textContent = "Draw !";
+}
